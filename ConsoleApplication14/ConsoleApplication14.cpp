c8f74a3ae9c17cd26fa483edf7308f12337a6f93
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-20 18:59:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    int N;
+    double distance = 1.5, sum = 0;
+    std::cout << "Enter the number of days: ";
+    std::cin >> N;
+
+    if (N == 0)
+        std::cout << "\nThe distance is: " << 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        sum += distance;
+        distance += 0.2;
+    }
+
+    std::cout << "\nThe distance is: " << sum << 'm';
+}
